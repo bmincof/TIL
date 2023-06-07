0b214ea4f609e8c72a2fc13b19455bd430c4bcbc
bmincof <104330984+bmincof@users.noreply.github.com>
2023-06-07 21:23:34 +0900

    [B5] 큰 수 A+B - 10757
    BigInteger 사용하는 문제

---
+++ CodingTest/BOJ/[B5] 큰 수 A+B - 10757.java
@@ -0,0 +1,15 @@
+package boj;
+
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        BigInteger a = new BigInteger(sc.next());
+        BigInteger b = new BigInteger(sc.next());
+        
+        System.out.println(a.add(b).toString());
+    }
+}
